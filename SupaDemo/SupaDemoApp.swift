0a6a72a5f10bd2cbafedcb07801a32375c3333b4
mkaulfers <matthew.kaulfers@yahoo.com>
2024-03-25 10:37:52 -0500

    Base Init

---
+++ SupaDemo/SupaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaDemoApp.swift
+//  SupaDemo
+//
+//  Created by Matthew Kaulfers on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SupaDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
